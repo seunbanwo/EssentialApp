af37fd5bc7b6811b4ecc6359fa31d31066fe3abf
Oluwaseun Adebanwo <findseun@gmail.com>
2023-02-18 18:22:21 +0100

    Create the feed item Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Seun Adebanwo on 18/02/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
