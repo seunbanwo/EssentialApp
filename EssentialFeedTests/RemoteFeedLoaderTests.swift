8112f7a380725afeafdfda96e8bb12bbbeea0427
Oluwaseun Adebanwo <findseun@gmail.com>
2023-02-19 08:40:21 +0100

    Refactor test to remove duplication

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -27,29 +27,27 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    // Make your class a subclass of the abstract class
-    // use composition instead of inheritance
-    
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
+        
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         
         // Use construstor injection
@@ -57,8 +55,18 @@ class RemoteFeedLoaderTests: XCTestCase {
         // or method injection not singleton
         let sut = RemoteFeedLoader(url: url, client: client)
         
-        sut.load()
+        // return tuple
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        // Make your class a subclass of the abstract class
+        // use composition instead of inheritance
         
-        XCTAssertEqual(client.requestedURL, url)
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
