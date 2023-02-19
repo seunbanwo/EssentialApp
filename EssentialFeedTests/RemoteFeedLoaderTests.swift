28c60a592f6a10b08ea61ddc01cbd7ed6fc9fbe8
Oluwaseun Adebanwo <findseun@gmail.com>
2023-02-19 08:05:53 +0100

    Make the HTTPClient a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,19 +8,53 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    // Use protocol instead of class.
+    // In protocol you dont have implementation just the definition
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
+    // Make your class a subclass of the abstract class
+    // use composition instead of inheritance
+    
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        
+        // Use construstor injection
+        // property injection
+        // or method injection not singleton
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
