96e46e4b979bb662d69cb97a4f99963795dae215
Oluwaseun Adebanwo <findseun@gmail.com>
2023-02-19 15:29:41 +0100

    Move `RemoteFeedLoader` to production

---
+++ EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -0,0 +1,47 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Seun Adebanwo on 19/02/2023.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    // Theres no need to make the HTTPClient a singleton
+    // or a shared instance, apart from convenience
+    // to locate the instance directly
+    // This is considered an anti pattern because it
+    // can introduce tight coupling use DI instead
+    // DI conforms to the open closed principle
+    // and keeps the design modular
+    //static var shared = HTTPCLient()
+
+    // Use protocol instead of class.
+    // This is just a contract defining which external functionality
+    // the remote feed loader needs.
+    // We dont need to create a new type to confirm it.
+    // We can easily create an extension on URLSession
+    // or AlamoFire to conform to the protocol
+    // This ensures our RemoteFeedLoader does not have to depend
+    // on concrete types like URLSession which makes it more flexible
+    // and open for extension
+    
+    // In protocol you dont have implementation just the definition
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    // By making final you prevent subclassing
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,44 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    let url: URL
-    let client: HTTPClient
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    // Theres no need to make the HTTPClient a singleton
-    // or a shared instance, apart from convenience
-    // to locate the instance directly
-    // This is considered an anti pattern because it
-    // can introduce tight coupling use DI instead
-    // DI conforms to the open closed principle
-    // and keeps the design modular
-    //static var shared = HTTPCLient()
-
-    // Use protocol instead of class.
-    // This is just a contract defining which external functionality
-    // the remote feed loader needs.
-    // We dont need to create a new type to confirm it.
-    // We can easily create an extension on URLSession
-    // or AlamoFire to conform to the protocol
-    // This ensures our RemoteFeedLoader does not have to depend
-    // on concrete types like URLSession which makes it more flexible
-    // and open for extension
-    
-    // In protocol you dont have implementation just the definition
-    func get(from url: URL)
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
     
