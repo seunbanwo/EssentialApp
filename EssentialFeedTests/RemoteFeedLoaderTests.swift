91f013be8ada18f6d824ab46c9cfd82f37e234f6
Oluwaseun Adebanwo <findseun@gmail.com>
2023-02-19 09:01:10 +0100

    Update Notes

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -22,7 +22,25 @@ class RemoteFeedLoader {
 }
 
 protocol HTTPClient {
+    // Theres no need to make the HTTPClient a singleton
+    // or a shared instance, apart from convenience
+    // to locate the instance directly
+    // This is considered an anti pattern because it
+    // can introduce tight coupling use DI instead
+    // DI conforms to the open closed principle
+    // and keeps the design modular
+    //static var shared = HTTPCLient()
+
     // Use protocol instead of class.
+    // This is just a contract defining which external functionality
+    // the remote feed loader needs.
+    // We dont need to create a new type to confirm it.
+    // We can easily create an extension on URLSession
+    // or AlamoFire to conform to the protocol
+    // This ensures our RemoteFeedLoader does not have to depend
+    // on concrete types like URLSession which makes it more flexible
+    // and open for extension
+    
     // In protocol you dont have implementation just the definition
     func get(from url: URL)
 }
@@ -47,7 +65,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(
+        url: URL = URL(string: "https://a-url.com")!
+    ) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         
         // Use construstor injection
