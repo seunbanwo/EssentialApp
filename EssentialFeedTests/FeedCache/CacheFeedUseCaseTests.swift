77882a637fb66f59a08dfb45b00f80a58fc9713b
Oluwaseun Adebanwo <oadebanwo@getcarbon.co>
2023-10-20 05:31:45 +0100

    Extract duplicate test helpers into a shared scope

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -123,22 +123,4 @@ class CacheFeedUseCaseTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
-    
-    private func uniqueImage() -> FeedImage {
-        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
-    }
-    
-    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
-        let models = [uniqueImage(), uniqueImage()]
-        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-        return (models, local)
-    }
-    
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
-    }
 }
