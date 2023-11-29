ce84ce787b947bbb08eb3e0f0a1634fa754e2f85
Oluwaseun Adebanwo <oadebanwo@getcarbon.co>
2023-11-29 19:04:21 +0100

    Localize feed view title string

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -5,6 +5,7 @@
 //  Created by Oluwaseun Adebanwo on 28/11/2023.
 //
 
+import Foundation
 import EssentialFeed
 
 protocol FeedLoadingView {
@@ -25,7 +26,10 @@ final class FeedPresenter {
     }
 
     static var title: String {
-        return "My Feed"
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                    tableName: "Feed",
+                    bundle: Bundle(for: FeedPresenter.self),
+                    comment: "Title for the feed view")
     }
     
     func didStartLoadingFeed() {
