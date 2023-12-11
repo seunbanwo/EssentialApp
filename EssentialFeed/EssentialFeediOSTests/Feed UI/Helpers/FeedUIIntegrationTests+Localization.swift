c9a3dae9d7a652e29d0aaed931ed313beb90ee0d
Oluwaseun Adebanwo <oadebanwo@getcarbon.co>
2023-12-11 03:10:44 +0100

    Replace `FeedPresenter` from iOS module with the `FeedPresenter` from the cross-platform EssentialFeed module

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests+Localization.swift
+//  FeedUIIntegrationTests+Localization.swift
 //  EssentialFeediOSTests
 //
 //  Created by Oluwaseun Adebanwo on 29/11/2023.
@@ -7,12 +7,12 @@
 
 import Foundation
 import XCTest
-import EssentialFeediOS
+import EssentialFeed
 
 extension FeedUIIntegrationTests {
     func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
+        let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
